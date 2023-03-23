1dd5632851bf34da6f96e571d4ea47d7441629f0
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2023-03-23 15:27:20 +0100

    Adds page styles

---
+++ src/styles/pages.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const StyledPage = styled.main`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+  border: 1px solid white;
+`;
