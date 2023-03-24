207dd1fee2679599479a3631a30bd8a0ec8950fe
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2023-03-24 14:42:14 +0100

    Adds button to card

--- src/components/cards/index.jsx
+++ src/components/cards/index.jsx
@@ -1,11 +1,13 @@
 import { StyledCard } from "../../styles/components/cards/cards.styles";
 import PrimaryCTA from "../buttons/primaryButton";
 
-export default function Card({ heading, text }) {
+export default function Card({ heading, text, img, altText, buttonText }) {
   return (
     <StyledCard>
+      <img src={img} alt={altText}></img>
       <h2>{heading}</h2>
-      <p>{text}</p>
+      <div>{text}</div>
+      <PrimaryCTA text={buttonText} />
     </StyledCard>
   );
 }
