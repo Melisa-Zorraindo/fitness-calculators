1c4e1ffcc6f0f4985b3f2c7e9f143182c1092c8c
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2023-03-24 17:37:07 +0100

    Renames and restructures files

---
+++ src/components/cards/LiftingCard.jsx
@@ -0,0 +1,30 @@
+import Card from ".";
+import { Link } from "react-router-dom";
+import weights from "../../assets/weights.png";
+
+export default function LiftingCard() {
+  return (
+    <Link to={"/liftingCalculator"}>
+      <Card
+        img={weights}
+        altText={"weights"}
+        heading={"Lifting calculator"}
+        text={
+          <>
+            <p>
+              Tired of guessing how much weight to lift during your workouts?
+              Use a lifting calculator!
+            </p>
+            <p>
+              Input your one-rep max and desired reps, and it generates the
+              perfect weight for your exercise. No guesswork, just efficient
+              workouts.
+            </p>
+            <p>Try it now and take your fitness game to the next level!</p>
+          </>
+        }
+        buttonText={"discover your goals"}
+      />
+    </Link>
+  );
+}
