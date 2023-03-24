05839e057006c716eb63d6caf24a88c7d7c94907
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2023-03-24 14:41:27 +0100

    Adds text

--- src/pages/calorieCalculator/index.jsx
+++ src/pages/calorieCalculator/index.jsx
@@ -1,12 +1,29 @@
 import Card from "../../components/cards";
 import { Link } from "react-router-dom";
+import meal from "../../assets/meal.png";
 
 export default function Calories() {
   return (
     <Link to={"/calorieCalculator"}>
       <Card
+        img={meal}
+        alt={"meal"}
         heading={"Calorie calculator"}
-        text={"This is how the calculator works"}
+        text={
+          <>
+            <p>
+              Are you struggling to meet your fitness goals? A calorie
+              calculator can help!
+            </p>
+            <p>
+              This tool calculates the number of calories you should consume
+              each day based on factors such as your age, weight, height, and
+              activity level.
+            </p>
+            <p>Try it out today and see the difference it can make!</p>
+          </>
+        }
+        buttonText={"start your journey"}
       />
     </Link>
   );
