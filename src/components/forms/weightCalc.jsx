d77f71e419b5b4671b2a860a1952cda0dc4289fe
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2023-03-25 14:05:14 +0100

    Adds instructions and styles interactions

--- src/components/forms/weightCalc.jsx
+++ src/components/forms/weightCalc.jsx
@@ -1,82 +1,120 @@
 import PrimaryCTA from "../buttons/primaryButton";
+import { Info } from "react-feather";
 
 export default function LiftingForm() {
   return (
-    <form>
-      <div className="field-block">
-        <label htmlFor="lifted-weight">I lifted</label>
-        <input
-          name="lifted-weight"
-          id="lifted-weight"
-          type="text"
-          placeholder="Enter amount in kg"
-          aria-label="Enter the amount of weight lifted in kilograms"
-        ></input>
-      </div>
+    <>
+      <button className="info-btn">
+        <Info size={"2rem"} />
+      </button>
+      <form>
+        <div className="field-block">
+          <label htmlFor="lifted-weight">I lifted</label>
+          <input
+            name="lifted-weight"
+            id="lifted-weight"
+            type="text"
+            placeholder="Enter amount in kg"
+            aria-label="Enter the amount of weight lifted in kilograms"
+          ></input>
+        </div>
 
-      <div className="field-block">
-        <label htmlFor="lifted-reps">For</label>
-        <input
-          name="lifted-reps"
-          id="lifted-reps"
-          type="number"
-          placeholder="Enter number of reps"
-          aria-label="Enter the number of reps you performed"
-        ></input>
-      </div>
+        <div className="field-block">
+          <label htmlFor="lifted-reps">For</label>
+          <input
+            name="lifted-reps"
+            id="lifted-reps"
+            type="number"
+            placeholder="Enter number of reps"
+            aria-label="Enter the number of reps you performed"
+          ></input>
+        </div>
 
-      <div className="field-block">
-        <label htmlFor="lifted-rpe">My RPE was</label>
-        <select
-          name="lifted-rpe"
-          id="lifted-rpe"
-          aria-label="Select the RPE value you experienced during the lift"
-        >
-          <option value="">Choose RPE</option>
-          <option>6.5</option>
-          <option>7</option>
-          <option>7.5</option>
-          <option>8</option>
-          <option>8.5</option>
-          <option>9</option>
-          <option>9.5</option>
-          <option>10</option>
-        </select>
-      </div>
+        <div className="field-block">
+          <label htmlFor="lifted-rpe">My RPE was</label>
+          <select
+            name="lifted-rpe"
+            id="lifted-rpe"
+            aria-label="Select the RPE value you experienced during the lift"
+          >
+            <option value="">Choose RPE</option>
+            <option>6.5</option>
+            <option>7</option>
+            <option>7.5</option>
+            <option>8</option>
+            <option>8.5</option>
+            <option>9</option>
+            <option>9.5</option>
+            <option>10</option>
+          </select>
+        </div>
 
-      <div className="field-block">
-        <label htmlFor="desired-reps">I want to endure</label>
-        <input
-          name="desired-reps"
-          id="desired-reps"
-          type="number"
-          placeholder="Enter number of reps"
-          aria-label="Enter the number of reps you want to perform"
-        ></input>
-      </div>
+        <div className="field-block">
+          <label htmlFor="desired-reps">I want to endure</label>
+          <input
+            name="desired-reps"
+            id="desired-reps"
+            type="number"
+            placeholder="Enter number of reps"
+            aria-label="Enter the number of reps you want to perform"
+          ></input>
+        </div>
 
-      <div className="field-block">
-        <label htmlFor="desired-rpe">At an RPE of</label>
-        <select
-          name="desired-rpe"
-          id="desired-rpe"
-          aria-label="Select the desired RPE value you want to perform the lift at"
-        >
-          <option>Choose RPE</option>
-          <option>6.5</option>
-          <option>7</option>
-          <option>7.5</option>
-          <option>8</option>
-          <option>8.5</option>
-          <option>9</option>
-          <option>9.5</option>
-          <option>10</option>
-        </select>
-      </div>
+        <div className="field-block">
+          <label htmlFor="desired-rpe">At an RPE of</label>
+          <select
+            name="desired-rpe"
+            id="desired-rpe"
+            aria-label="Select the desired RPE value you want to perform the lift at"
+          >
+            <option>Choose RPE</option>
+            <option>6.5</option>
+            <option>7</option>
+            <option>7.5</option>
+            <option>8</option>
+            <option>8.5</option>
+            <option>9</option>
+            <option>9.5</option>
+            <option>10</option>
+          </select>
+        </div>
 
-      <div className="btn-block">
         <PrimaryCTA text={"calculate"} />
-      </div>
-    </form>
+        <div className="instructions">
+          <ol>
+            <li>
+              Enter the weight you lifted in kilograms (kg) in the
+              <span> "I lifted" </span>
+              field.
+            </li>
+            <li>
+              Enter the number of reps you completed in the <span> "For" </span>
+              field.
+            </li>
+            <li>
+              Select the RPE (Rate of Perceived Exertion) you experienced from
+              the dropdown menu in the <span> "My RPE was" </span>field.
+            </li>
+            <li>
+              Enter the number of reps you want to endure in the{" "}
+              <span> "I want to endure" </span>field.
+            </li>
+            <li>
+              Select the desired RPE you want to achieve from the dropdown menu
+              in the<span> "At an RPE of" </span>field.
+            </li>
+            <li>
+              Click the<span> "Calculate" </span>button to generate the
+              recommended weight to lift for the desired number of reps and RPE.
+            </li>
+          </ol>
+          <p>
+            <span> Note: </span>The RPE scale ranges from 6.5 (very light) to 10
+            (maximum effort), with increments of 0.5. Choose the RPE that most
+            closely matches your perceived level of exertion during the lift.
+          </p>
+        </div>
+      </form>
+    </>
   );
 }
--- src/styles/components/forms/calc.styles.jsx
+++ src/styles/components/forms/calc.styles.jsx
@@ -3,17 +3,61 @@ import styled from "styled-components";
 export const StyledForm = styled.div`
   background-color: ${({ theme }) => theme.color.support};
   color: ${({ theme }) => theme.color.main};
-  border-radius: 1.5rem;
+  border-radius: 0.8rem;
   padding: 2rem;
   margin: 0 auto;
+  position: relative;
+
+  .info-btn {
+    position: absolute;
+    top: -2rem;
+    width: 4rem;
+    height: 4rem;
+    background: ${({ theme }) => theme.color.accent};
+    border: none;
+    border-radius: 50%;
+    cursor: pointer;
+    z-index: 1;
+  }
+
+  .instructions {
+    position: absolute;
+    top: 0;
+    left: 0;
+    border-radius: 0.8rem;
+    padding: 2rem 1rem;
+    background: ${({ theme }) => theme.color.onFocus};
+    display: none;
+    height: 100%;
+
+    ol {
+      padding: 1rem;
+    }
+
+    li {
+      padding: 0.5rem 0;
+
+      span {
+        font-weight: 700;
+      }
+    }
+
+    p {
+      span {
+        font-weight: 700;
+      }
+    }
+  }
 
   form {
     width: 100%;
+    display: flex;
+    flex-direction: column;
 
     .field-block {
       display: flex;
       flex-direction: column;
-      margin: 3rem 0;
+      margin: 2rem 0;
       font-size: 16px;
       font-weight: 700;
 
@@ -27,13 +71,9 @@ export const StyledForm = styled.div`
 
         &:focus {
           outline: none;
-          border-bottom: 2px solid ${({ theme }) => theme.color.onFocus};
+          border-bottom: 2px solid ${({ theme }) => theme.color.onFocusContrast};
         }
       }
-    }
-
-    .btn-block {
-      text-align: center;
     }
   }
 `;
