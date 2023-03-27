b3caabe3f2870cb645cdbd544dd4ffca15ec3fde
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2023-03-27 15:38:32 +0200

    Adds calorie form validation

--- src/components/forms/calorieCalc.jsx
+++ src/components/forms/calorieCalc.jsx
@@ -1,20 +1,75 @@
 import PrimaryCTA from "../buttons/primaryButton";
 import { Info } from "react-feather";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const schema = yup
+  .object({
+    gender: yup.string().required("This is a required field"),
+    age: yup
+      .number()
+      .typeError("This is a required field")
+      .positive()
+      .integer()
+      .min(18, "Age cannot be lower than 18")
+      .max(80, "Age cannot be higher than 80")
+      .required(),
+    personWeight: yup
+      .number()
+      .typeError("This is a required field")
+      .positive()
+      .integer()
+      .min(40, "Weight cannot be lower than 40KG")
+      .max(160, "Weight cannot be higher than 160KG")
+      .required(),
+    personHeight: yup
+      .number()
+      .typeError("This is a required field")
+      .positive()
+      .integer()
+      .min(130, "Height cannot be lower than 130CM")
+      .max(230, "Height cannot be higher than 230CM")
+      .required(),
+    activityLevel: yup.string().required("This is a required field"),
+    goal: yup.string().required("This is a required field"),
+  })
+  .required();
 
 export default function CalorieForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  function onSubmit(data) {
+    console.log(data);
+  }
+
   return (
     <>
       <button className="info-btn">
         <Info size={"2rem"} />
       </button>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className="field-block">
           <label htmlFor="gender">Your gender</label>
-          <select name="gender" id="gender" aria-label="Select your gender">
+          <select
+            name="gender"
+            id="gender"
+            aria-label="Select your gender"
+            {...register("gender", {
+              required: true,
+            })}
+          >
             <option value="">Choose gender</option>
             <option>Female</option>
             <option>Male</option>
           </select>
+          <p>{errors.gender?.message}</p>
         </div>
         <div className="field-block">
           <label htmlFor="age">Your age</label>
@@ -24,17 +79,21 @@ export default function CalorieForm() {
             type="number"
             placeholder="Enter your age"
             aria-label="Enter your age"
+            {...register("age", { required: true, min: 18, max: 80 })}
           ></input>
+          <p>{errors.age?.message}</p>
         </div>
         <div className="field-block">
-          <label htmlFor="person-weigth">Your weight</label>
+          <label htmlFor="person-weight">Your weight</label>
           <input
-            name="person-weigth"
-            id="person-weigth"
+            name="person-weight"
+            id="person-weight"
             type="number"
             placeholder="Enter your weight in KG"
             aria-label="Enter your weight in KG"
+            {...register("personWeight", { required: true, min: 40, max: 160 })}
           ></input>
+          <p>{errors.personWeight?.message}</p>
         </div>
         <div className="field-block">
           <label htmlFor="person-height">Your height</label>
@@ -44,7 +103,13 @@ export default function CalorieForm() {
             type="number"
             placeholder="Enter your height in CM"
             aria-label="Enter your height in CM"
+            {...register("personHeight", {
+              required: true,
+              min: 130,
+              max: 230,
+            })}
           ></input>
+          <p>{errors.personHeight?.message}</p>
         </div>
         <div className="field-block">
           <label htmlFor="activity-level">Your activity level</label>
@@ -52,22 +117,30 @@ export default function CalorieForm() {
             name="activity-level"
             id="activity-level"
             aria-label="Select your activity level"
+            {...register("activityLevel", { required: true })}
           >
-            <option>Choose activity level</option>
-            <option>Sedentary (office job)</option>
-            <option>Light exercise (1-2 days a week)</option>
-            <option>Moderate exercise (3-5 days a week)</option>
-            <option>Heavy exercise (6-7 days a week)</option>
+            <option value={""}>Choose activity level</option>
+            <option value={2}>Sedentary (office job)</option>
+            <option value={3}>Light exercise (1-2 days a week)</option>
+            <option value={4}>Moderate exercise (3-5 days a week)</option>
+            <option value={5}>Heavy exercise (6-7 days a week)</option>
           </select>
+          <p>{errors.activityLevel?.message}</p>
         </div>
         <div className="field-block">
           <label htmlFor="goal">Your goal</label>
-          <select name="goal" id="goal" aria-label="Select your goal">
-            <option>Choose goal</option>
-            <option>Lose weight</option>
-            <option>Maintain weight</option>
-            <option>Gain weight</option>
+          <select
+            name="goal"
+            id="goal"
+            aria-label="Select your goal"
+            {...register("goal", { required: true })}
+          >
+            <option value={""}>Choose goal</option>
+            <option value={"mildlose"}>Lose weight</option>
+            <option value={"maintain"}>Maintain weight</option>
+            <option value={"mildgain"}>Gain weight</option>
           </select>
+          <p>{errors.goal?.message}</p>
         </div>
         <PrimaryCTA text={"calculate"} />
         <div className="instructions">
