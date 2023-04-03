9382d7c57cc6b4f7a518b2eb1d4b249b01c79a4c
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2023-04-03 17:21:20 +0200

    Fixes percentages error

--- src/utils/calorieCalculatorFunctions/macroSplit/ketoSplitCalc.js
+++ src/utils/calorieCalculatorFunctions/macroSplit/ketoSplitCalc.js
@@ -1,6 +1,6 @@
 export default function ketoSplitCalc(tdee) {
   const protein = Math.floor((tdee * 0.15) / 4);
-  const fat = Math.floor((tdee * 0.7) / 9);
+  const fat = Math.floor((tdee * 0.75) / 9);
   const carbs = Math.floor((tdee * 0.1) / 4);
 
   return {
