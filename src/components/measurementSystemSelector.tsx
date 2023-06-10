db25691698c28b0d5474eb06a21a46629e8e9466
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2023-06-10 08:15:57 +0200

    Adds types to system selector fixing bug

--- src/components/measurementSystemSelector.tsx
+++ src/components/measurementSystemSelector.tsx
@@ -1,23 +1,37 @@
+import { ChangeEvent } from "react";
 import { StyledMeasurementSystemSelector } from "../styles/components/measurementSystemSelector.styles";
 
-export default function MeasurementSystemSelector({ system, setSystem }) {
-  function handleSystemChange(event) {
-    setSystem(event.target.id);
+export default function MeasurementSystemSelector({
+  system,
+  setSystem,
+}: {
+  system: string;
+  setSystem: (system: string) => void;
+}) {
+  //function to handle measuring system change
+  function handleSystemChange(event: ChangeEvent<HTMLInputElement>) {
+    setSystem(event.currentTarget.id);
   }
 
   return (
-    <StyledMeasurementSystemSelector onChange={handleSystemChange}>
+    <StyledMeasurementSystemSelector>
       <div>
         <input
           type="radio"
           name="system"
           id="metric"
           defaultChecked={system === "metric"}
+          onChange={handleSystemChange}
         ></input>
         <label htmlFor="metric">Metric</label>
       </div>
       <div>
-        <input type="radio" name="system" id="imperial"></input>
+        <input
+          type="radio"
+          name="system"
+          id="imperial"
+          onChange={handleSystemChange}
+        ></input>
         <label htmlFor="imperial">Imperial</label>
       </div>
     </StyledMeasurementSystemSelector>
