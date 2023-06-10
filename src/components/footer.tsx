cee41940f1fce924bc509d88cb040106d913dc13
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2023-06-10 14:21:16 +0200

    Adds typescript annotations for layout and icon links

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,16 +1,23 @@
 import { StyledFooter } from "../styles/components/footer.styles";
 import IconLink from "./iconLinks";
 import { GitHub, Linkedin, Mail } from "react-feather";
+import { IconProps } from "./iconLinks";
 
 export default function Footer(): JSX.Element {
   return (
     <StyledFooter>
-      <IconLink icon={GitHub} linkTo={"https://github.com/Melisa-Zorraindo"} />
       <IconLink
-        icon={Linkedin}
+        icon={GitHub as React.ComponentType<IconProps>}
+        linkTo={"https://github.com/Melisa-Zorraindo"}
+      />
+      <IconLink
+        icon={Linkedin as React.ComponentType<IconProps>}
         linkTo={"https://www.linkedin.com/in/melisa-zorraindo-81719618b/"}
       />
-      <IconLink icon={Mail} linkTo={"mailto:melisa.zorraindo@gmail.com"} />
+      <IconLink
+        icon={Mail as React.ComponentType<IconProps>}
+        linkTo={"mailto:melisa.zorraindo@gmail.com"}
+      />
     </StyledFooter>
   );
 }
--- src/components/iconLinks/index.tsx
+++ src/components/iconLinks/index.tsx
@@ -1,7 +1,17 @@
 import { StyledIconLink } from "../../styles/components/iconLinks.styles";
 import { Link } from "react-router-dom";
+import { ComponentType } from "react";
 
-export default function IconLink({ icon: Icon, linkTo }) {
+export interface IconProps {
+  size?: number;
+  color?: string;
+}
+interface IconLinkProps {
+  icon: ComponentType<IconProps>;
+  linkTo: string;
+}
+
+export default function IconLink({ icon: Icon, linkTo }: IconLinkProps) {
   return (
     <Link to={linkTo} target="_blank">
       <StyledIconLink>{<Icon size={16} />}</StyledIconLink>
